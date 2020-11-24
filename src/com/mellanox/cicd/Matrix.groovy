22fccad54a740c0caf17ad26944bfb8cb1b92f62
Mike Dubman <miked@mellanox.com>
2020-11-24 23:00:29 +0200

    add support for ngci

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -249,7 +249,7 @@ def run_step(config, script, title, shell) {
     run_shell("echo Starting step: ${title}", title)
     if (shell == "ngci") {
         config.logger.debug("xxx Running ngci step with shell="+script)
-        def ngci = library(identifier: 'ngci@ci_version-3.1',
+        library(identifier: 'ngci@ci_version-3.1',
                             retriever: modernSCM([$class: 'GitSCMSource', 
                                         remote: 'http://l-gerrit.mtl.labs.mlnx:8080/DevOps/Jenkins/ci_framework']))
 
@@ -260,7 +260,7 @@ def run_step(config, script, title, shell) {
  //       return gShell.evaluate(script)
         env.SPRING_APPLICATION_JSON = '{"blackduck.url":"https://blackduck.mellanox.com/","blackduck.api.token":"ODMwOWYwMzEtODA2ZC00MzBjLWI1ZDEtNmFiMjBkYzQzMzkwOjNmNjExN2M1LWE2ZmEtNDZlYS1hZjRiLTZlNDgwNjAwOTVjNw=="}'
 
-        ngci.NGCIBlackDuckScan (projectName: "ci-demo",
+        NGCIBlackDuckScan (projectName: "ci-demo",
                         projectVersion: "1.0",
                         projectSrcPath: "${env.WORKSPACE}/src",
                         attachArtifact: true ,
