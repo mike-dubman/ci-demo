03a8fd18e5c91d05c3093c6bf7c84bb0e7bf1054
Mike Dubman <miked@mellanox.com>
2020-11-24 22:55:34 +0200

    add support for ngci

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -253,11 +253,20 @@ def run_step(config, script, title, shell) {
                             retriever: modernSCM([$class: 'GitSCMSource', 
                                         remote: 'http://l-gerrit.mtl.labs.mlnx:8080/DevOps/Jenkins/ci_framework']))
 
-        def vars = [:]
-        vars['ngci'] = ngci
-        vars['env'] = env
-        GroovyShell gShell = new GroovyShell(new Binding(vars))
-        return gShell.evaluate(script)
+//        def vars = [:]
+//        vars['ngci'] = ngci
+//        vars['env'] = env
+ //       GroovyShell gShell = new GroovyShell(new Binding(vars))
+ //       return gShell.evaluate(script)
+        env.SPRING_APPLICATION_JSON = '{"blackduck.url":"https://blackduck.mellanox.com/","blackduck.api.token":"ODMwOWYwMzEtODA2ZC00MzBjLWI1ZDEtNmFiMjBkYzQzMzkwOjNmNjExN2M1LWE2ZmEtNDZlYS1hZjRiLTZlNDgwNjAwOTVjNw=="}'
+
+        ngci.NGCIBlackDuckScan (projectName: "ci-demo",
+                        projectVersion: "1.0",
+                        projectSrcPath: "${env.WORKSPACE}/src",
+                        attachArtifact: true ,
+                        reportName: "BlackDuck report",
+                        scanMode: "source")        
+        return;
     }
 
     def cmd = """${shell}
