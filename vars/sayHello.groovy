5e91250c19182feb02c6ec589ce04a006b58e862
Mike Dubman <miked@mellanox.com>
2020-11-25 17:16:55 +0200

    add support for ngci

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-    echo "mmmmmm Hello, ${name}."
+def call(String arg1 = 'aaaa', String arg2 = 'bbbb') {
+    echo "mmmmmm Hello ${arg1} -- ${arg2} ."
 }
