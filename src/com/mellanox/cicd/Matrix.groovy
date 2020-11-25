c50ce83e46458e333d8b471690af1e26e8efc213
Mike Dubman <miked@mellanox.com>
2020-11-25 16:57:40 +0200

    add support for ngci

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -258,8 +258,7 @@ def run_step(config, script, title, shell) {
  //       vars['env'] = env
  //       GroovyShell gShell = new GroovyShell(new Binding(vars))
  //       return gShell.evaluate(script)
-          def args = ['momomo']
-          this."${script}"(*args)
+          this."${script}"('helllllllllo')
           return
 //        env.SPRING_APPLICATION_JSON = '{"blackduck.url":"https://blackduck.mellanox.com/","blackduck.api.token":"ODMwOWYwMzEtODA2ZC00MzBjLWI1ZDEtNmFiMjBkYzQzMzkwOjNmNjExN2M1LWE2ZmEtNDZlYS1hZjRiLTZlNDgwNjAwOTVjNw=="}'
 
