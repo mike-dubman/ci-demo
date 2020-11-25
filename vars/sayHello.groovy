6abbae4097fa5db0f26ac1250dca1d12d727570f
Mike Dubman <miked@mellanox.com>
2020-11-25 19:43:31 +0200

    add support for ngci

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-def ngci = library(identifier: 'ngci@ci_version-3.1',
+library(identifier: 'ngci@ci_version-3.1',
                    retriever: modernSCM([$class: 'GitSCMSource', 
                    remote: 'http://l-gerrit.mtl.labs.mlnx:8080/DevOps/Jenkins/ci_framework']))
 
@@ -9,7 +9,7 @@ def call(projectName, projectVersion, projectSrcPath, attachArtifact, reportName
 
 //        env.SPRING_APPLICATION_JSON = '{"blackduck.url":"https://blackduck.mellanox.com/","blackduck.api.token":"ODMwOWYwMzEtODA2ZC00MzBjLWI1ZDEtNmFiMjBkYzQzMzkwOjNmNjExN2M1LWE2ZmEtNDZlYS1hZjRiLTZlNDgwNjAwOTVjNw=="}'
 
-    ngci.NGCIBlackDuckScan (
+    NGCIBlackDuckScan (
         projectName: "ci-demo",
         projectVersion: "1.0",
         projectSrcPath: "${env.WORKSPACE}/src",
