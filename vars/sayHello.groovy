4195609730f39ff86e8419073a7641347010b5c9
Mike Dubman <miked@mellanox.com>
2020-11-25 18:34:35 +0200

    add support for ngci

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(projectName, projectVersion, projectSrcPath, attachArtifact, reportName, scanMode) {
-    echo "mmmmmm Hello ${projectName} -- ${projectVersion}"
+    echo "mmmmmm Hello ${projectName} -- ${projectVersion} -- last: $scanMode"
 }
