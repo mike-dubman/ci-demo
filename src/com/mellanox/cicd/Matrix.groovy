2ccda0ef03185afbfaf7199f3405bf0c8346bd98
Mike Dubman <miked@mellanox.com>
2020-11-27 21:02:21 +0200

    support for select_by

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -254,7 +254,7 @@ def run_step(image, config, title, oneStep) {
         skip++
     }
 
-    def customSel = oneStep.get("container_select_by")
+    def customSel = oneStep.get("containerSelector")
     if (customSel != null && matchMapEntry(customSel, image)) {
         config.logger.debug("step name='" + oneStep.name + "' requests container with attr=" + customSel)
         skip--
