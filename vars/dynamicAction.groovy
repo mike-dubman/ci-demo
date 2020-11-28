c71a0679555119e410f4349eb9464c566a80821a
Mike Dubman <miked@mellanox.com>
2020-11-28 17:42:19 +0200

    support for select_by

--- vars/dynamicAction.groovy
+++ vars/dynamicAction.groovy
@@ -3,6 +3,11 @@
 def call(List args) {
 
     println("==>DynamicAction(" + args + ")")
+
+    if (args.size() < 1) {
+        println("fatal: DynamicAction() expects at least 1 parameter")
+        sh("false")
+    }
     def actionName = args[0]
     def actionScript = libraryResource "actions/${actionName}"
     def toFile = new File (env.WORKSPACE + "/cidemo_" + actionName)
@@ -11,6 +16,6 @@ def call(List args) {
     writeFile(file: toFile.getAbsolutePath(), text: actionScript)
     sh("chmod +x " + toFile.getAbsolutePath())
 
-    sh(toFile.getAbsolutePath() + args)
+    sh(toFile.getAbsolutePath() + " " + args.subList(1,args.size()).join(" "))
     return;
 }
