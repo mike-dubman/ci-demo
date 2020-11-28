ee8e352d428f8ba8ec74d611c96e6e4f6c7b8a6a
Mike Dubman <miked@mellanox.com>
2020-11-28 16:40:21 +0200

    support for select_by

--- vars/dynamicAction.groovy
+++ vars/dynamicAction.groovy
@@ -6,8 +6,10 @@ def call(actionName, preCmd, buildCmd) {
     println("==>DynamicAction ($actionName, $preCmd, $buildCmd)")
     def actionScript = libraryResource "actions/${actionName}"
     def toFile = new File ("cidemo_" + actionName)
+
+    println("action file: " + toFile.getAbsolutePath())
     toFile.write("${actionScript}")
-    sh("chmod 755 " + toFile.getAbsolutePath())
+    sh("chmod +x " + toFile.getAbsolutePath())
 
     sh(toFile.getAbsolutePath() + " $buildCmd")
     return;
