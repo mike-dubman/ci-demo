e9c35ee31a015416056430f7bc48c082633c4e96
Mike Dubman <miked@mellanox.com>
2020-12-04 19:56:08 +0200

    rename to ngci

---
+++ vars/ngci.groovy
@@ -0,0 +1,24 @@
+#!/usr/bin/env groovy
+
+def call(List args) {
+
+    library(identifier: 'ngci@ci_version-3.1',
+            retriever: modernSCM([$class: 'GitSCMSource', 
+            remote: 'http://l-gerrit.mtl.labs.mlnx:8080/DevOps/Jenkins/ci_framework']))
+
+    println("==>ngci(" + args + ")")
+
+    if (args.size() < 1) {
+        println("fatal: DynamicAction() expects at least 1 parameter")
+        sh("false")
+    }
+    def actionName = args[0]
+
+    if (args.size() > 1) {
+        cmd += " " + args.subList(1,args.size()).collect{ "'" + it + "'"}.join(" ")
+    }
+    println("Running " + cmd)
+    "$actionName"(cmd)
+
+    return;
+}
