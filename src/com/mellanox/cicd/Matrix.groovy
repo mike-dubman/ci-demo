759f00607803eef5bf62ee03d14397d601242dde
Mike Dubman <miked@mellanox.com>
2020-12-10 08:58:54 +0200

    support for select_by

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -726,9 +726,10 @@ def run_parallel_in_chunks(config, myTasks, bSize) {
     }
 
     config.logger.trace(3, "run_parallel_in_chunks: batch size is ${bSize}")
-
+    
+    parallel myTasks.subMap(myTasks)
   //  (myTasks.keySet() as List).collate(bSize).each {
-        parallel myTasks.subMap(it)
+  //      parallel myTasks.subMap(it)
   //  }
 }
 
