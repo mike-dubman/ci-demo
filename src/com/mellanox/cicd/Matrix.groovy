e22d2ebef629fcdfa9b4ec7a70a25f8a668c90d5
Mike Dubman <miked@mellanox.com>
2020-12-13 21:22:28 +0200

    support for select_by

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -742,10 +742,14 @@ def run_parallel_in_chunks(config, myTasks, bSize) {
 def loadConfigFile(filepath, logger) {
 
     logger.debug("loadConfigFile: path=" + filepath)
-    def config = readYAML(file: filepath)
-    def rawFile = readFile(filepath)
+    def config = readYaml(file: filepath)
+    def rawFile = readFile(fifilepath)
 
     logger.debug("loadConfigFile:\n" + rawFile)
+
+    writeJSON(config, ".ci/koko")
+    config = readJSON(file: ".ci/koko")
+
 
     if (config.get("matrix")) {
         if (config.matrix.include != null && config.matrix.exclude != null) {
