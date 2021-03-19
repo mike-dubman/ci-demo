6eb8519dc1b0390d076a8f2568b08c0fee47563a
Mike Dubman <miked@nvidia.com>
2021-03-19 13:43:28 +0200

    support for select_by

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -108,7 +108,7 @@ def run_step_shell(cmd, title, oneStep, config) {
 
         attachArtifacts(config, oneStep.archiveArtifacts)
         attachJunit(config, oneStep.archiveJunit)
-        attachTap(config, config.archiveTap)
+        attachTap(config, oneStep.archiveTap)
 
         if (ret.rc != 0) {
             def msg = "Step ${title} failed with exit code=${ret.rc}"
