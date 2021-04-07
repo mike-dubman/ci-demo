182e32741d93b87fa3b26a2c1dd3d6755aae6c6f
Mike Dubman <miked@nvidia.com>
2021-04-07 20:18:10 +0300

    support for select_by

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -429,7 +429,7 @@ def check_skip_stage(image, config, title, oneStep, axis) {
 
     def stepEnabled = getConfigVal(config, ['enable'], true, true, oneStep)
 
-    if (stepEnabled) {
+    if (!stepEnabled) {
         config.logger.trace(2, "Step '${oneStep.name}' is disabled in project yaml file, skipping")
         return true
     }
