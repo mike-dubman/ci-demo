1542789f9205aa3a4e765d9e6506ac00d1b3a23c
Mike Dubman <miked@nvidia.com>
2021-06-21 13:02:29 +0300

    support for select_by

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -436,7 +436,7 @@ def check_skip_stage(image, config, title, oneStep, axis) {
         return true
     }
 
-    def gSelectors = [config.runs_on_dockers, config.runs_on_agents]
+
     def selectors = [oneStep.containerSelector, oneStep.agentSelector]
     def skip = false
 
@@ -448,8 +448,8 @@ def check_skip_stage(image, config, title, oneStep, axis) {
         selector = selectors[i]
         if (selector && selector.size() > 0) {
             def customSel = stringToList(selector)
-            config.logger.trace(2, "xxx Selector=" + selector + " custom=" + customSel + " gSel=" + stringToList(gSelectors[i]))
-            if (matchMapEntry(customSel, stringToList(gSelectors[i])) && matchMapEntry(customSel, axis)) {
+            config.logger.trace(2, "xxx Selector=" + selector + " custom=" + customSel + " name=" + image.name)
+            if (matchMapEntry(customSel, axis)) {
                 config.logger.trace(2, "Step '" + oneStep.name + " matched with axis=" + axis + " selector=" + selector)
                 skip = false
                 break
